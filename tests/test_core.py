4fd7442a59eab5eaf40c120b36e1b498c4c71707
Henning <henning.holgersen@gmail.com>
2024-02-18 16:06:16 +0100

    test fix

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,13 +1,20 @@
 """Misc tests"""
 
-from dbt_autodoc.utils import get_column_descriptions, parse_manifest, get_upstream_models
-
-def test_column_descriptions():
-  manifest = parse_manifest('tests/data/manifest_1.json')
-  col_desc = list(get_column_descriptions('customers', manifest))
-  assert len(col_desc) == 2
+from dbt_autodoc.utils import (
+  get_upstream_models, 
+  parse_manifest, 
+  get_upstream_models,
+  compile_upstream_description_markdown
+)
 
 def test_upstream_models():
   manifest = parse_manifest('tests/data/manifest_1.json')
   upstream_models = get_upstream_models('customers', manifest)
   assert len(upstream_models) == 1
+
+def test_compile_upstream_description_markdown():
+  manifest = parse_manifest('tests/data/manifest_1.json')
+  upstream_models = get_upstream_models('customers', manifest)
+  assert len(upstream_models) == 1
+  upstream_description = compile_upstream_description_markdown('customers', manifest)
+  assert "The name of the customer" in upstream_description 
